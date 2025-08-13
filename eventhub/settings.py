bf23e601efab2edd2042eb3e8088abef8155cb0a
andrej-dev-new <ritmasivna@gmail.com>
2025-08-13 23:41:00 +0300

    Update settings.py
    changed allowed hosts to '127.0.0.1' and 'localhost' both

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'dev-only-use-env-in-prod'
 DEBUG = False
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
